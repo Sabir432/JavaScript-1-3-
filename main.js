068d83906737ff1bf6870bb31dd89ae1d553f411
Sabir <444@gmail.com>
2024-08-18 10:57:09 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = prompt('Введите своё имя')
+let age = prompt('Введите свой возрост')
+
+let a = +prompt('10 + 2 =')
+let a1 = +prompt('10 - 2 =')
+let a2 = +prompt('10 / 2 =')
+let a3 = +prompt('10 * 2 =')
+
+console.log('10 + 2: Ответ ' + 12 +` Вы ввели ${a}`)
+console.log('10 - 2: Ответ ' + 8 +` Вы ввели ${a1}`)
+console.log('10 / 2: Ответ ' + 10 / 2 +` Вы ввели ${a2}`)
+console.log('10 * 2: Ответ ' + 10 * 2 +` Вы ввели ${a3}`)
